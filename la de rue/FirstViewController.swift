7406b4757cf469c2777897e021398e7157e927f0
Spencer Bull <spencerbull2554@gmail.com>
2015-07-11 15:07:22 -0500

    Initial Commit

---
+++ la de rue/FirstViewController.swift
@@ -0,0 +1,53 @@
+//
+//  FirstViewController.swift
+//  la de rue
+//
+//  Created by Spencer Bull on 7/11/15.
+//  Copyright (c) 2015 bullapse. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let textCellIdentifier = "TextCell"
+    
+    let swiftBlogs = ["Ray Wenderlich", "NSHipster", "iOS Developer Tips", "Jameson Quave", "Natasha The Robot", "Coding Explorer", "That Thing In Swift", "Andrew Bancroft", "iAchieved.it", "Airspeed Velocity"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // MARK:  UITextFieldDelegate Methods
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return swiftBlogs.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as! UITableViewCell
+        
+        let row = indexPath.row
+        cell.textLabel?.text = swiftBlogs[row]
+        
+        return cell
+    }
+    
+    // MARK:  UITableViewDelegate Methods
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let row = indexPath.row
+        println(swiftBlogs[row])
+    }
+}
+
---
+++ la de rueTests/la_de_rueTests.swift
@@ -0,0 +1,36 @@
+//
+//  la_de_rueTests.swift
+//  la de rueTests
+//
+//  Created by Spencer Bull on 7/11/15.
+//  Copyright (c) 2015 bullapse. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class la_de_rueTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
